198437997fbcc41ca6b9ffd49c8401880483d547
Alfred <105510070+AlfredUFY@users.noreply.github.com>
2024-01-17 12:58:09 +0000

    update

--- app/(components)/Hero.js
+++ app/(components)/Hero.js
@@ -2,7 +2,7 @@ import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 export default function Hero() {
     return (
-        <div className="relative isolate overflow-hidden bg-gray-900">
+        <div className="relative isolate overflow-hidden bg-gray-900 min-h-screen">
             <svg
                 className="absolute inset-0 -z-10 h-full w-full stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
                 aria-hidden="true"
@@ -78,13 +78,12 @@ export default function Hero() {
                 </div>
                 <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                     <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-                        <img
-                            src="#"
+                        <div
                             alt="App screenshot"
                             width={2432}
                             height={1442}
-                            className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-                        />
+                            className="w-[76rem] lg:h-[720px] h-[450px] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
+                        ></div>
                     </div>
                 </div>
             </div>
